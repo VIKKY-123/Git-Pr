c4c8fd246fd81509aa48f0a23c23561df28dea81
DELL <DELL@DESKTOP-S3RV3MH>
2022-03-08 12:17:59 +0530

    add the new classes

---
+++ src/test/java/com/Merge/Merge1.java
@@ -0,0 +1,8 @@
+package com.Merge;
+
+public class Merge1 {
+	public static void main(String[] args) {
+		System.out.println("this is first Merge");
+
+	}
+}
---
+++ src/test/java/com/Merge/Merge2.java
@@ -0,0 +1,8 @@
+package com.Merge;
+
+public class Merge2 {
+	public static void main(String[] args) {
+		System.out.println("this is second Merge");
+
+	}
+}
